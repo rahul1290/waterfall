75c56231ddd3de631dd8d31d4c27b0cdbb01f9da
unknown <rahul.sinha@innodeed.com>
2022-09-21 22:44:27 +0530

    waterfall test

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const async = require('async');
+
+const app = express()
+const port = 3000
+
+const fun1 = (callback) =>{
+    console.log("fun1 called");
+    const x = 12;
+    return callback(null,x)
+}
+
+
+const fun2 = (payload,callback) =>{
+    console.log("fun2 called");
+    return callback(null)
+}
+
+const fun3 = (payload,callback) =>{
+    console.log("fun3 called");
+    return payload
+}
+
+const fun4 = (payload,callback) =>{
+    console.log("fun4 called");
+    console.log(payload);
+    return null
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+const x = 2;
+
+async.waterfall([
+    async.apply(fun1),
+    function(payload,callback){
+        if (x == 2) {
+            async.waterfall([
+                async.apply(fun3,payload,callback),
+                fun4
+            ],callback)
+        } else{
+            async.waterfall([
+                fun2
+            ],callback)
+        }
+    },
+],(error) => {
+    console.log('error:',error)
+})
